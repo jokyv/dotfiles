947064916c0bfbf7e2dd1b6af07005abce6d7c68
John Kyvetos <johnkyvetos@gmail.com>
2024-08-01 15:45:50 +0800

    updates

--- bin/fzf_go_to_path.sh
+++ bin/fzf_go_to_path.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
+#! /bin/bash
 
 # find all type directories from $HOME and choose where to cd next
 # fd -i for enabling case-sensitive
-fzf_go_to_path() {
-    cd "$(fd -td -H -i . $HOME | fzf)" &&
-    eza --color=always --icons=always --long --all --group-directories-first --git
-}
+# fzf_go_to_path() {
+#     cd "$(fd -td -H -i . $HOME | fzf)" &&
+#     eza --color=always --icons=always --long --all --group-directories-first --git
+# }
 
-fzf_go_to_path()
+fzf_go_to_path() {
+    local chosen_dir
+    chosen_dir=$(fd -td -H -i . $HOME | fzf) && 
+    cd "$chosen_dir" &&
+    eza --color=always --icons=always --long --all --group-directories-first --git &&
+    cd "$chosen_dir"
+} 
+
+fzf_go_to_path
