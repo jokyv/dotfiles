260c7fba2edd347a92e22520878467115d5fbd65
John Kyvetos <johnkyvetos@gmail.com>
2024-08-06 14:19:28 +0800

    feat(home manager nix): moved to the unstable

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -3,11 +3,16 @@
   description = "My Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.05";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"
-    # home-manager.url = "github:nix-community/home-manager";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    # stable
+    # nixpkgs.url = "nixpkgs/nixos-24.05";
+    # home-manager.url = "github:nix-community/home-manager/release-24.05";
+
+    # unstable
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # compile helix from source
     # helix.url = "github:helix-editor/helix"; 
   };
 
@@ -16,14 +21,15 @@
       system = "x86_64-linux";
       lib = nixpkgs.lib;
       pkgs = import nixpkgs { inherit system; };
-    in {
+    in
+    {
       homeConfigurations = {
         my_profile = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [ ./home.nix ];
           # modules = [ ./home.nix ./helix.nix];
           # extraSpecialArgs = {helix-flake = helix;      };
+        };
       };
     };
-  };
 }
