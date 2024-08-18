c7a683bf6c07d1b513683f5fc3f252b1c64a1cda
John Kyvetos <johnkyvetos@gmail.com>
2024-08-18 12:00:44 +0800

    feat(bravebookmarks.sh): replaced rofi for fuzzel

--- bin/bravebookmarks.sh
+++ bin/bravebookmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-selected=$(cat ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks | grep '"url":' | awk '{print $2}' | sed 's/"//g' | rofi -dmenu -p "Select a Brave Bookmark")
+selected=$(cat ~/.config/BraveSoftware/Brave-Browser/Default/Bookmarks | grep '"url":' | awk '{print $2}' | sed 's/"//g' | fuzzel -d -p "Select a Brave Bookmark")
 
 if [ "$selected" ]; then
     brave $selected
