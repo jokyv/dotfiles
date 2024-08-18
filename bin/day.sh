d7986044fd08a1e2b58ceaf6bdebd8b680242fc7
John Kyvetos <johnkyvetos@gmail.com>
2024-08-18 22:27:47 +0800

    refactor(day.sh): small refactoring

--- bin/day.sh
+++ bin/day.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-# SCRIPTS
-# ----------------------------------------------------------------------------
+# SCRIPT
 # a simple script that prints the date to the format i want for my notes
+# ------------------------------------------------
+
 day() {
   date_now=$(date +"%A, %d %B %Y")
   echo "$date_now"
