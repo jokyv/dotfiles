521c88652bb343f5d11a1ccfdadb0a0380b054f0
John Kyvetos <johnkyvetos@gmail.com>
2024-09-02 06:57:58 +0800

    feat(linux_util): created new python script for linux related scripts

--- bin/bash_util.sh
+++ bin/bash_util.sh
@@ -2,7 +2,7 @@
 
 # Here all my re-usable bash functions
 # If you want to use on another bash script simply do the following:
-# source ~/dot/bin/bash_util.sh
+# source ~/dot/bin/bash_util.sh inside another .sh file
 # then run the function normally as pause_for_user
 
 # Ask the enter before continue to the next section of the script
@@ -12,4 +12,23 @@ pause_for_user() {
     read -p $'\e[1;33mPress Enter to continue to the next part\e[0m'
     echo -e "\e[1;36m----------------------------------------\e[0m"
     echo
+}
+
+# a helper function when a section is of a script is 'disabled'
+work_in_progress() {
+  echo ""
+  echo 'this section is work in progress'
+  echo ""
+}
+
+# Ask Y/n
+function ask() {
+    read -p "$1 (Y/n): " resp
+    if [ -z "$resp" ]; then
+        response_lc="y" # empty is Yes
+    else
+        response_lc=$(echo "$resp" | tr '[:upper:]' '[:lower:]') # case insensitive
+    fi
+
+    [ "$response_lc" = "y" ]
 }
