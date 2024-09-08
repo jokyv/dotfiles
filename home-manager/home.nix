d114f02942e0953adda0549819752f6cfa428e12
John Kyvetos <johnkyvetos@gmail.com>
2024-09-08 22:26:27 +0800

    feat(home-manager): updates

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -25,14 +25,44 @@
   stylix.polarity = "dark";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/everforest.yaml";
 
-  # install packages AND specify their configs
-  # gtk settings
-  # gtk = {
-  #   enable = true;
-  #   theme.name = "adw-gtk3";
-  #   cursorTheme.name = "Bibata-Modern-Ice";
-  #   iconTheme.name = "GruvboxPlus";
+  stylix.cursor.package = pkgs.bibata-cursors;
+  stylix.cursor.name = "Bibata-Modern-Ice";
+
+  stylix.targets.gtk.enable = true;
+  stylix.targets.alacritty.enable = true;
+  stylix.targets.mako.enable = true;
+  stylix.targets.swaylock.enable = true;
+  stylix.targets.yazi.enable = true;
+  stylix.targets.zathura.enable = true;
+  stylix.targets.firefox.enable = true;
+  stylix.targets.fuzzel.enable = true;
+  # stylix.targets.bat.enable = true;
+  # stylix.targets.kitty.enable = true;
+  # stylix.targets.fzf.enable = true;
+  # stylix.targets.helix.enable = true;
+
+  # stylix.theme = {
+  #   gtk.enable = true;
+  #   gtk.iconTheme = {
+  #     name = "everforest";
+  #     package = pkgs.everforest-gtk-theme;
+  #   };
   # };
+
+  # gtk settings
+  #   gtk = {
+  #   enable = true;
+  #   iconTheme = {
+  #     # name = "Papirus-Dark";
+  #     # package = pkgs.papirus-icon-theme;
+  #     # name = "Tela";
+  #     # package = pkgs.tela-icon-theme;
+  #     name = "everforest";
+  #     package = pkgs.everforest-gtk-theme;
+  #   };
+  # };
+
+  # install packages AND specify their configs
   #
   # network settings
   # networking = {
@@ -61,7 +91,30 @@
   # };
 
   programs.home-manager.enable = true;
+  programs.alacritty.enable = true;
 
-  # programs.helix.enable = true;
-  # programs.helix.settings.theme = "autumn_night_transparent";
+  # Create specific folders in home directory
+  # home.file = {
+  #   "downloads".source = config.lib.file.mkOutOfStoreSymlink "/home/${config.home.username}/downloads";
+  #   "projects".source = config.lib.file.mkOutOfStoreSymlink "/home/${config.home.username}/projects";
+  #   "pics".source = config.lib.file.mkOutOfStoreSymlink "/home/${config.home.username}/pics";
+  # };
+
+  # Set XDG user directories
+  xdg.userDirs = {
+    enable = true;
+
+    createDirectories = false;
+    desktop = "${config.home.homeDirectory}";
+    download = "${config.home.homeDirectory}/downloads";
+    pictures = "${config.home.homeDirectory}/pics";
+    templates = "${config.home.homeDirectory}";
+    publicShare = "${config.home.homeDirectory}";
+    documents = "${config.home.homeDirectory}";
+    music = "${config.home.homeDirectory}";
+    videos = "${config.home.homeDirectory}";
+    extraConfig = {
+      XDG_PROJECTS_DIR = "${config.home.homeDirectory}/projects";
+    };
+  };
 }
