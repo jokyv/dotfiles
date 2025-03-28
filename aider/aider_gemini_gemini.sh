767aa8b9e48ea40c113b4f135f6aa76825915e9b
John Kyvetos <johnkyvetos@gmail.com>
2025-03-28 09:16:31 +0800

    Chore: Comment out unavailable gemini-2.5 model in vertex_ai

--- aider/aider_gemini_gemini.sh
+++ aider/aider_gemini_gemini.sh
@@ -8,10 +8,15 @@ aider \
   --weak-model vertex_ai/gemini-2.0-flash-exp \
   --editor-edit-format whole
 
-# newest config
+# newest config but gemini 2.5 not available in vertex right now
 # aider \
 #   --architect \
-#   --model vertex_ai/gemini-2.5-pro \
+#   --model vertex_ai/gemini-2.5-pro-exp-03-25 \
 #   --editor-model vertex_ai/gemini-2.0-pro-exp-02-05 \
 #   --weak-model vertex_ai/gemini-2.0-flash-exp \
 #   --editor-edit-format whole
+
+# testing
+# aider \
+#   --model vertex_ai/gemini-2.5-pro-exp-03-25 \
+#   --editor-edit-format diff-fenced
