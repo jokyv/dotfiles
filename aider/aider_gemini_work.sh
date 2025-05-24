36e323efbb4a1a5fc2aa85237e1437dae1bf4f75
John Kyvetos <johnkyvetos@gmail.com>
2025-05-24 16:42:29 +0800

    chore: Add weak Gemini model to aider script

--- aider/aider_gemini_work.sh
+++ aider/aider_gemini_work.sh
@@ -6,5 +6,7 @@
 # Experimental is always free
 aider \
   --model vertex_ai/gemini-2.5-pro-preview-05-06 \
+  --weak-model vertex_ai/gemini-2.5-flash-preview-05-20 \
   --edit-format udiff-simple
+  # --model vertex_ai/gemini-2.5-flash-preview-04-17 \
   # --editor-edit-format diff-fenced
